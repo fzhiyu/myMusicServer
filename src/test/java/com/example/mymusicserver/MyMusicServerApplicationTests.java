c98861e92b357f3061980046feec70f883177ef1
fzhiyu <2017015437@qq.com>
2022-03-07 20:25:25 +0800

    初始

---
+++ src/test/java/com/example/mymusicserver/MyMusicServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mymusicserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMusicServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
